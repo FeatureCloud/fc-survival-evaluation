eeb0fea8b15df64292a725e49327a25279c864cb
Zeno Sewald <zsewa@outlook.de>
2021-09-03 17:38:30 +0200

    Fix set objective attribute in init

--- app/logic.py
+++ app/logic.py
@@ -48,6 +48,8 @@ class AppLogic:
         self.y_test_filename = None
         self.y_pred_filename = None
 
+        self.objective = None
+
         self.sep = ","
         self.mode = None
         self.dir = "."
